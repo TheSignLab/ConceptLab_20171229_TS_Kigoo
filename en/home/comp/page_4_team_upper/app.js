d33353f0b8527417fdd5705941a8f33f321dc5eb
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-01-05 15:55:21 -0500

    Fixes #1

--- en/home/comp/page_4_team_upper/app.js
+++ en/home/comp/page_4_team_upper/app.js
@@ -5,15 +5,19 @@ $(document).ready(function () {
 	$('#id_trigger_p1').mouseenter(function () {
 		$('#id_bubble_p1').fadeIn("slow");
 		$('#id_bubble_p1').addClass('animated bounceInUp');
+		clearTimeout(ClockOffP1_Timer);
 	})
+	
 	$('#id_trigger_p1').mouseleave(function (e) {
 
 		ClockOffP1_Timer = setTimeout(function () {
 			$('#id_bubble_p1').fadeOut("slow");
-		}, 1000);
+		}, 3000);
 
 	})
-
+ 
+        
+	
 	$('#id_bubble_p1').mouseenter(function () {
 		clearTimeout(ClockOffP1_Timer);
 	})
@@ -21,7 +25,7 @@ $(document).ready(function () {
 	$('#id_bubble_p1').mouseleave(function () {
 		ClockOffP1_Timer = setTimeout(function () {
 			$('#id_bubble_p1').fadeOut("slow");
-		}, 1000);
+		}, 3000);
 	})
 
 
